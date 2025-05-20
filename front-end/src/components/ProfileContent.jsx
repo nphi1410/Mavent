53f9aeba00797b84b6bcd82aafc182f1c018bbc3
ndkhoi192 <ndkhoi192@gmail.com>
2025-05-20 16:54:10 +0700

    Update ProfileContent.jsx

--- front-end/src/components/ProfileContent.jsx
+++ front-end/src/components/ProfileContent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ProfileContent.module.css';
+import styles from './css/ProfileContent.module.css'; // Adjust the path as necessary
 
 const ProfileContent = () => {
   const userData = {
