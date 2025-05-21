82fd2a8d4f36d557e731995bae5d9c47ffafd628
MinhAnhNek <nguyenminan20001011@gmail.com>
2025-05-21 12:49:20 +0700

    format code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,22 +2,22 @@ import { useEffect, useState } from 'react';
 import { getGreeting } from './services/testService';
 
 function App() {
-  const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    getGreeting()
-      .then(response => setMessage(response.data))
-      .catch(error => {
-        console.error('Error fetching greeting:', error);
-        setMessage('Failed to load message');
-      });
-  }, []);
+    useEffect(() => {
+        getGreeting()
+            .then(response => setMessage(response.data))
+            .catch(error => {
+                console.error('Error fetching greeting:', error);
+            setMessage('Failed to load message');
+        });
+    }, []);
 
-  return (
-    <>
-      <h1>{message}</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>{message}</h1>
+        </>
+    );
 }
 
 export default App;
