0c131abc440aae2675370766bceffc75d4b1088c
ndkhoi192 <ndkhoi192@gmail.com>
2025-05-22 14:35:45 +0700

    chore: update ProfilePage

--- front-end/src/pages/ProfilePage.jsx
+++ front-end/src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SideBar from '../components/SideBar';
+import SideBar from '../components/SidebarSettings';
 import ProfileContent from '../components/ProfileContent';
 import Header from '../components/Header';
 
