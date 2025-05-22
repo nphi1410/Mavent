5ba3b3e9e0e6018b3c6dd540e7ce71cf416a264a
nvdekay <khanhnvd.work@gmail.com>
2025-05-22 14:09:11 +0700

    feat: add base dashboard view

---
+++ front-end/src/pages/SuperAdminDashBoard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt, faUser, faClock, faArrowUpRight } from "@fortawesome/free-solid-svg-icons";
+
+function SuperAdminDashBoard() {
+    const stats = [
+        {
+            title: 'Total Events',
+            icon: 
+        }
+    ]
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SuperAdminDashBoard
