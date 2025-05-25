25c39f93278cfacdaf2e2003360e77418682259f
nvdekay <khanhnvd.work@gmail.com>
2025-05-25 21:30:34 +0700

    add: add entity event

---
+++ back-end/src/main/java/com/mavent/dev/entity/Event.java
@@ -0,0 +1,47 @@
+package com.mavent.dev.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "events") // Tuỳ chọn: nếu bạn muốn đặt tên bảng rõ ràng
+@Data
+@NoArgsConstructor
+@ToString
+public class Event {
+
+    @Id
+    @Column(name = "event_id", nullable = false, unique = true)
+    private String eventId;
+
+    @Column(name = "event_name")
+    private String eventName;
+
+    @Column(name = "event_description")
+    private String eventDescription;
+
+    @Column(name = "event_location")
+    private String eventLocation;
+
+    @Column(name = "event_start_date")
+    private LocalDate eventStartDate;
+
+    @Column(name = "event_end_date")
+    private LocalDate eventEndDate;
+
+    @Column(name = "event_status")
+    private String eventStatus;
+
+    @Column(name = "event_d_day_info")
+    private String eventDDayInfo;
+
+    @Column(name = "event_max_member")
+    private int eventMaxMember;
+
+    @Column(name = "event_max_participant")
+    private String eventMaxParticipant; // Nếu là số, hãy dùng int
+}
