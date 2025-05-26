8312526b6a5a42ee74fe7b4ec392ce84064d83c5
nvdekay <khanhnvd.work@gmail.com>
2025-05-26 14:45:56 +0700

    chore: clean code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route
+    BrowserRouter as Router,
+    Routes,
+    Route
 } from "react-router-dom";
 import ProfilePage from './pages/ProfilePage';
 import Login from './pages/Login';
