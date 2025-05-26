b05b873b632ec5cd8262f15867a541bc385163c6
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2025-05-26 14:45:19 +0700

    fix:fix route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,42 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import ProfilePage from './pages/ProfilePage';
-import Login from './pages/Login';
-import HomePage from './pages/HomePage';
-import SuperAdminDashboard from './pages/SuperAdminDashBoard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import SuperAdminDashboard from "./pages/SuperAdminDashBoard";
 
 function App() {
+  return (
+    <Router>
+      <div className="min-h-screen w-full bg-gray-100">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile/*" element={<ProfilePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/accounts" element={<AccountList />} />
+          <Route path="/admin" element={<SuperAdminDashboard />} />
 
-    return (
-        <Router>
-            <div className="h-screen w-screen flex bg-amber-50">
-                <SuperAdminSidebar />
-                <div className="flex flex-col flex-1">
-                    <SuperAdminHeader />
-
-                    <main className="flex-1 overflow-y-auto p-10 bg-gray-100">
-                        <Routes>
-                            {/* Route for Dashboard */}
-                            <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
-
-                            {/* Route for view events */}
-                            <Route path="/superadmin/events" element={<SuperAdminViewEvents />} />
-
-                            <Route path="/superadmin" element={
-                                <>
-                                    <SuperAdminDashboard />
-                                </>
-                            }
-                            />
-                        </Routes>
-                    </main>
-                </div>
-            </div>
-        </Router>
-    );
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
