acdc482f68c6df990b68a030eb2bb69d93624c11
nvdekay <khanhnvd.work@gmail.com>
2025-05-26 15:04:34 +0700

    chore: install dependences

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,11 @@ import {
 import ProfilePage from './pages/ProfilePage';
 import Login from './pages/Login';
 import HomePage from './pages/HomePage';
+import SuperAdminSidebar from './components/SuperAdminSidebar';
+import SuperAdminHeader from './components/SuperAdminHeader';
 import SuperAdminDashboard from './pages/SuperAdminDashBoard';
+import SuperAdminViewEvents from './pages/SuperAdminViewEvents';
+import SuperAdminManageUsers from './pages/SuperAdminManageUsers';
 
 function App() {
 
@@ -19,18 +23,15 @@ function App() {
 
                     <main className="flex-1 overflow-y-auto p-10 bg-gray-100">
                         <Routes>
-                            {/* Route for Dashboard */}
-                            <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
-
-                            {/* Route for view events */}
-                            <Route path="/superadmin/events" element={<SuperAdminViewEvents />} />
-
                             <Route path="/superadmin" element={
                                 <>
                                     <SuperAdminDashboard />
                                 </>
                             }
                             />
+                            <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
+                            <Route path="/superadmin/events" element={<SuperAdminViewEvents />} />
+                            <Route path="/superadmin/events" element={<SuperAdminManageUsers />} />
                         </Routes>
                     </main>
                 </div>
