82f29bb02734e6c7ec21434ee1c9ff7fba8eeeb6
nvdekay <khanhnvd.work@gmail.com>
2025-05-27 17:10:19 +0700

    chore: fix logo mavent

--- front-end/src/components/SuperAdminHeader.jsx
+++ front-end/src/components/SuperAdminHeader.jsx
@@ -7,9 +7,8 @@ function SuperAdminHeader() {
     <header className="h-16 border-b border-gray-200 flex items-center justify-between px-6 fixed top-0 left-0 right-0 bg-white">
       {/* Left section */}
       <div className="flex items-center gap-6">
-        <image src="/mavent-logo.svg" alt="Mavent Logo" className="w-12 h-12" />
         <div className="flex items-center justify-center p-4 text-xl font-bold text-gray-800">
-          Super Admin
+          <img src="/mavent-text-logo.svg" alt="mavent" className="w-40" />
         </div>
       </div>
 
