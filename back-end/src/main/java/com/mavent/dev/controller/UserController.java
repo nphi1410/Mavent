4abcae835d248a839a4e2e5d6a2702578c832e7e
MinhAnhNek <nguyenminan20001011@gmail.com>
2025-05-27 14:32:49 +0700

    chore: fix lack of "Autowired" keyword

--- back-end/src/main/java/com/mavent/dev/controller/UserController.java
+++ back-end/src/main/java/com/mavent/dev/controller/UserController.java
@@ -21,6 +21,8 @@ public class UserController {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
     private AccountService accountService;
 
     @PostMapping("/login")
