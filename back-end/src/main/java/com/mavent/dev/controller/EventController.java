6df4d61333145a22c3fe74b24051ccd3dd72ef03
nvdekay <khanhnvd.work@gmail.com>
2025-05-29 14:57:57 +0700

    feat: create getEvenById function to read data each event

--- back-end/src/main/java/com/mavent/dev/controller/EventController.java
+++ back-end/src/main/java/com/mavent/dev/controller/EventController.java
@@ -1,23 +1,28 @@
 package com.mavent.dev.controller;
 
-import com.mavent.dev.entity.Event;
+import com.mavent.dev.DTO.EventDTO;
 import com.mavent.dev.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EventController {
+
     @Autowired
     private EventService eventService;
 
+    //Lấy tất cả sự kiện
     @GetMapping("/events")
-    public List<Event> getAllEvents() {
+    public List<EventDTO> getAllEvents() {
         return eventService.getAllEvents();
+    }
+
+    //Lấy chi tiết sự kiện theo ID
+    @GetMapping("/events/{id}")
+    public EventDTO getEventById(@PathVariable("id") Integer eventId) {
+        return eventService.getEventById(eventId);
     }
 }
