9afbf2edfc3628eaa0f5de7e414e43c3c8945b9b
ndkhoi192 <ndkhoi192@gmail.com>
2025-05-31 00:57:48 +0700

    update: update profileservice, add api

--- front-end/src/services/profileService.jsx
+++ front-end/src/services/profileService.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Api from "../config/Api";
 
 const API_URL = 'http://localhost:8080/api';
@@ -25,4 +26,14 @@ export const uploadAvatar = (formData) => {
       'Content-Type': 'multipart/form-data',
     },
   });
+};
+
+export const getUserEvents = async () => {
+  try {
+    const response = await axiosInstance.get('/user/events');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user events:', error);
+    throw error; // Throw error để component có thể handle
+  }
 };
