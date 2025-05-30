69ce45505b96bfb700a5505a4130036a0c03e5e0
ndkhoi192 <ndkhoi192@gmail.com>
2025-05-30 20:12:23 +0700

    update: update service

--- front-end/src/services/profileService.jsx
+++ front-end/src/services/profileService.jsx
@@ -1,14 +1,28 @@
 import Api from "../config/Api";
 
+const API_URL = 'http://localhost:8080/api';
 
-export const getUserProfile = () => Api.get("/user/profile");
-
-export const updateProfile = (data) => Api.put("/user/profile", data);
-
-export const uploadAvatar = (formData) => Api.post("/user/avatar", formData, {
-    headers: {
-        'Content-Type': 'multipart/form-data'
-    }
+// Create axios instance with default config
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, // Important for cookies/session
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
-export const getAllAccounts = () => Api.get("/accounts");
+export const getUserProfile = () => {
+  return axiosInstance.get('/user/profile');
+};
+
+export const updateProfile = (data) => {
+  return axiosInstance.put('/user/profile', data);
+};
+
+export const uploadAvatar = (formData) => {
+  return axiosInstance.post('/user/avatar', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
