8b6e0cde922bec604fc4d4da7ea8bb089bea7951
ndkhoi192 <ndkhoi192@gmail.com>
2025-05-31 00:58:23 +0700

    feat: add user event page

---
+++ front-end/src/pages/UserEventsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserEventsContent from '../components/usercenter/UserEventsContent';
+import Sidebar from '../components/usercenter/SidebarSettings';
+
+const UserEventsPage = () => {
+  return (
+    <div className="flex h-screen w-full bg-gray-100">
+      <Sidebar />
+      <UserEventsContent />
+    </div>
+  );
+};
+
+export default UserEventsPage;
