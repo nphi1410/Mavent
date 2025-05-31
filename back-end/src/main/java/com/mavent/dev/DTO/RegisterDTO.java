8d523371270573540124c3d02434c64db857756b
MinhAnhNek <nguyenminan20001011@gmail.com>
2025-05-31 21:14:30 +0700

    Feat: add file RegisterDTO

---
+++ back-end/src/main/java/com/mavent/dev/DTO/RegisterDTO.java
@@ -0,0 +1,12 @@
+package com.mavent.dev.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDTO {
+    private String username;
+    private String password;
+    private String email;
+}
