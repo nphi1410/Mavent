fe8f8e556be9ab05f012321b52703c5256353ad3
MinhAnhNek <nguyenminan20001011@gmail.com>
2025-05-31 21:14:46 +0700

    style: change css of Register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,7 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8080/api/register", {
+    // const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
+
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, email, password }),
+      credentials: 'include', 
+    });
+
+    if (response.ok) {
+      // redirect to home page
+      window.location.href = "/login";
+    } else {
+      console.error("Login failed");
+    }
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-900">
@@ -12,7 +37,7 @@ const Register = () => {
         <h2 className="text-4xl md:text-5xl font-semibold text-center mb-6 text-blue-900 pb-3">
           REGISTER NEW ACCOUNT
         </h2>
-        
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-7 pb-3">
 
           <div className="flex flex-col">
@@ -21,14 +46,14 @@ const Register = () => {
             <input 
               type="text"   
               placeholder="Enter username..." 
-              className="p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-700" 
+              className="pl-4 p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-700" 
             />
             
             <label className="mb-1 mt-4 text-sm font-medium text-gray-700 pl-2.5">Email:</label>
             <input 
               type="email" 
               placeholder="Enter email..." 
-              className="p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
+              className="pl-4 p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
               />
           </div>
 
@@ -38,14 +63,14 @@ const Register = () => {
             <input 
               type="password" 
               placeholder="Enter password..." 
-              className="p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
+              className="pl-4 p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
             />
 
             <label className="mb-1 mt-4 text-sm font-medium text-gray-700 pl-2.5">Confirm password:</label>
             <input 
             type="password" 
             placeholder="Confirm password..." 
-            className="p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
+            className="pl-4 p-2 border border-blue-400 rounded-full focus:outline-none focus:ring-1 focus:ring-blue-500" 
             />
 
           </div>
