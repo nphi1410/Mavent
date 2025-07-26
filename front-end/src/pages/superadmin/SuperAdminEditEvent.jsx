0070527b9edd2a14270f8c346bff59f23a26b9ac
MinhAnhNek <anhntmhe194390@gmail.com>
2025-07-27 01:19:36 +0700

    chore: fix Camel

--- front-end/src/pages/superadmin/SuperAdminEditEvent.jsx
+++ front-end/src/pages/superadmin/SuperAdminEditEvent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Avatar } from "../../components/ui/avatar";
+import { Avatar } from "../../components/ui/Avatar";
 import { getAssignedAdmin, updateRole } from "../../services/RoleService";
 import { getEventById, updateEvent } from "../../services/eventService";
 import { getAllLocations } from "../../services/EventLocationService";
